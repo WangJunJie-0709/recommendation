e05267d329e3168ec68c06f75f7f9a7c0fd9e147
王俊杰 <wangjunjie@xsyxsc.com>
2024-06-07 14:19:47 +0800

    update ruozhiba data

--- test.py
+++ test.py
@@ -1,7 +1,7 @@
 import asyncio
 import time
 from typing import List
-
+import os
 import aiotieba as tb
 from aiotieba import PostSortType
 from aiotieba.logging import get_logger as LOG
@@ -11,18 +11,13 @@ BDUSS = "dETjN3TjVuT3MwdzJhTjV0V3dtbE92ckVZdWJ6Y1V4emxtYUMtVzNGcER5SDltSUFBQUFBJ
 thread_list = []
 reply_list = []
 
-async def crawler(fname: str):
-    """
-    获取贴吧名为fname的贴吧的前32页中浏览量最高的10个主题帖
+meaning_less = ['出院', '弱智吧']
 
-    Args:
-        fname (str): 贴吧名
-    """
+
+async def crawler(fname: str):
 
     start_time = time.perf_counter()
     LOG().info("Spider start")
-
-    # thread_list用来保存主题帖列表
 
 
     # 使用键名"default"对应的BDUSS创建客户端
@@ -39,7 +34,7 @@ async def crawler(fname: str):
             生产者协程
             """
 
-            for pn in range(100, 0, -1):
+            for pn in range(10000000, 0, -1):
                 # 生产者使用Queue.put不断地将页码pn填入任务队列task_queue
                 await task_queue.put(pn)
             # 这里需要nonlocal来允许对闭包外的变量的修改操作（类似于引用传递和值传递的区别）
@@ -72,29 +67,53 @@ async def crawler(fname: str):
                         return
                 else:
                     # 执行被分派的任务，即爬取pn页的帖子列表
-                    threads = await client.get_threads(fname, pn, is_good=True)
-
+                    threads = await client.get_threads(fname, pn)
+                    skip_thread = False
                     for thread in threads:
-                        replys = await client.get_posts(thread.tid, rn=100, sort=PostSortType.HOT, with_comments=True)
-                        title = thread.title if thread.title != '' else thread.contents.objs[0].text
-                        tmp = []
-                        for reply in replys:
+                        if skip_thread:
+                            skip_thread = False
 
-                            if reply.contents.objs == []:
+                        if thread.user.level < 4 or thread.contents.objs == [] or not isinstance(thread.contents.objs[0], tb.api._classdef.contents.FragText):
+                            # 用户等级大于等于4, 且帖子标题为文本类型
+                            continue
+
+                        title = thread.title if thread.title != '' else thread.contents.objs[0].text
+                        replys = await client.get_posts(thread.tid, rn=100)
+                        for word in meaning_less:  # 过滤包含无意义词的帖子
+                            if word in title:
+                                skip_thread = True
+                                break
+
+                        if skip_thread:
+                            continue  # 跳过当前thread的剩余处理
+                        tmp = []
+
+                        for reply in replys:
+                            if reply.contents.objs == [] or not isinstance(reply.contents.objs[0], tb.api._classdef.contents.FragText):
+                                # 回复类型需要为文本类型
                                 continue
-                            if not isinstance(reply.contents.objs[0], tb.api._classdef.contents.FragText):
+                            if reply.user.user_id == thread.user.user_id and title != reply.contents.objs[0].text:
+                                # 如果回复和标题的用户是同一个人, 将其拼接
+                                title = title + '。' + reply.contents.objs[0].text
                                 continue
+                            reply_num = reply.reply_num
+                            disagree = reply.disagree
+                            agree = reply.agree
                             answer = reply.contents.objs[0].text
+                            if answer in meaning_less or len(answer) == 1 or answer == title:
+                                # 过滤无意义回复
+                                continue
+
                             print("The title is " + f"{title}" " and the reply is " + f"{answer}")
-                            tmp.append(answer)
+                            tmp.append([answer, agree, disagree, reply_num])
                         thread_list.append(title)
                         reply_list.append(tmp)
                     # 这里的nonlocal同样是为了修改闭包外的变量thread_list
                     # nonlocal thread_list
                     # nonlocal reply_list
 
-        # 创建8个消费者协程
-        workers = [worker(i) for i in range(8)]
+        # 创建消费者协程
+        workers = [worker(i) for i in range(512)]
 
         # 使用asyncio.gather并发执行
         # 需要注意这里*workers中的*意为将列表展开成多个参数
@@ -103,33 +122,27 @@ async def crawler(fname: str):
 
 
     LOG().info(f"Spider complete. Time cost: {time.perf_counter()-start_time:.4f} secs")
-    print(len(thread_list))
-    print(len(reply_list))
-    # # 按主题帖浏览量降序排序
-    # thread_list.sort(key=lambda thread: thread.view_num, reverse=True)
-    # print(len(thread_list))
-    # print(len(reply_list))
-    # # 将浏览量最高的10个主题帖的信息打印到日志
-    # for i, thread in enumerate(thread_list[0:10], 1):
-    #     # print(thread)
-    #     tid = thread.tid
-    #     pid = thread.pid
-    #     title = thread.title
-    #     view_num = thread.view_num
-    #     reply_num = thread.reply_num
-    #     share_num = thread.share_num
-    #     agree_num = thread.agree
-    #     disagree_num = thread.disagree
-    #
-    #     if title == '':
-    #         title = thread.contents.objs[0].text
-    #     # print('title: ', title)
-    #     # pn = await asyncio.wait_for(task_queue.get(), timeout=1)
-    #     # replys = await client.get_replys()
-    #     # print('replys: ', replys)
-    #
-    #     # LOG().info(f"Rank#{i} view_num:{thread.view_num} title:{thread.title}")
 
+    # 筛选出优质回复
+    for reply in reply_list:
+        reply[:] = sorted(reply, key=lambda x: (-x[1], x[2], -x[3]))[:min(5, len(reply))]
+
+    paired_data = [
+        (thread, reply[0], reply[1], reply[2], reply[3])  # 分别对应title, reply, agree, disagree, reply_num
+        for thread, replies in zip(thread_list, reply_list)
+        for reply in replies
+        if reply[1] >= 10
+    ]
+
+    df = pd.DataFrame(paired_data, columns=['title', 'Reply', 'agree', 'disagree', 'reply_num'])
+
+    df = df.drop_duplicates()
+    csv_file_path = 'ruozhiba.csv'
+
+    if os.path.exists(csv_file_path):
+        os.remove(csv_file_path)
+
+    df.to_csv(csv_file_path, index=False, encoding='utf-8-sig')
 
 # 执行协程crawler
 asyncio.run(crawler("弱智"))
